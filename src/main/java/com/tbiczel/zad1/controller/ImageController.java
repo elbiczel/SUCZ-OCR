d542d114e5c0a100409624526bac70dd2f3cc3ba
Tomasz Biczel <el.biczel@gmail.com>
2011-02-27 02:06:06 +0100

    Controller improved.

--- src/main/java/com/tbiczel/zad1/controller/ImageController.java
+++ src/main/java/com/tbiczel/zad1/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.tbiczel.zad1.controller;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,12 +9,17 @@ import javax.imageio.ImageIO;
 import com.tbiczel.zad1.gui.ImagePanel;
 
 public class ImageController {
+	
+	private ImagePanel panel;
+	
+	private BufferedImage image;
 
 	public ImagePanel read(File file) {
 		try {
-			ImagePanel image = new ImagePanel();
-			image.setImg(ImageIO.read(file));
-			return image;
+			panel = new ImagePanel();
+			image = ImageIO.read(file);
+			panel.setImg(image);
+			return panel;
 		} catch (IOException e) {
 			return null;
 		}
